2e120e75904b5d4ee396fc68d8e91af1dcce5e9f
rGharco <gabrielradulescu05@protonmail.com>
2024-12-31 19:22:36 +0200

    Improvements and code clarification

--- main.py
+++ main.py
@@ -2,30 +2,29 @@ constSpecialCharacters = {"!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+"
 
 def caesarEncryption(message="",key=0):
     encryptedMessage = ""
-    for i in message:
-        # We want the ASCII code for the character that is why we convert to bytes first and then use indexing
-        # We make all letters in lower to reduce complexity and treating two separate ranges of letters at once
-        currentCharASCII = bytes(str(i).lower(), 'utf-8')
+    for char in message:
         
-        if i in constSpecialCharacters:
-            encryptedMessage += i
+        if char in constSpecialCharacters:
+            encryptedMessage += char
             continue
-            
-        # The ASCII code range for letters is A=65 and z=122, the check is so that we dont get characters like {}[]% etc
-        # if the current letter + the key is bigger than 122 (e.g in the case of y + 3)
-        elif (currentCharASCII[0] + key) > 122:
-            if i == str(i).lower():
-                i = 'a'
-            else:
-                i = 'A'
-            i = chr(ord(i)+(key-currentCharASCII[0] % 122-1))
-        else:
-            i = chr(ord(i)+key)
         
-        encryptedMessage += i
+        elif (ord(char.lower()) + key) > 122:
+            charBeforeTransformation = ord(char.lower())
+            char = ord('a') if char.lower() == char else ord('A')
+            char = chr(char + (key - (122 - charBeforeTransformation)))
+            
+        else:
+            char = chr(ord(char)+key)
+        
+        encryptedMessage += char
         
     return encryptedMessage
 
-print("[==================================================================================]\n")
+print("\n[==================================================================================]\n")
 
-print(caesarEncryption("Z bc!",4))
+message = str(input("Enter a message: "))
+key = int(input("Enter a numerical key: "))
+print("\nCaesar Encryption: ", caesarEncryption(message,key))
+
+
+print("\n[==================================================================================]\n")
